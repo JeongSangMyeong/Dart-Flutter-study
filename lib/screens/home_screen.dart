026b9dbd9b5c6daa0578b7b613e9c78cc7a4d0bb
JeongSangMyeong <ajflsp@naver.com>
2024-07-03 17:23:28 +0900

    타이머 멈추는 메소드 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
   late Timer timer;
 
   // 1초씩 뺌
@@ -20,11 +21,23 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  // 타이머 시작 메소드
   void onStartPressed() {
     timer = Timer.periodic(
       const Duration(seconds: 1),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  // 타이머 멈추는 메소드
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -52,8 +65,10 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: onStartPressed,
-                icon: const Icon(Icons.play_circle_outline),
+                onPressed: isRunning ? onPausePressed : onStartPressed,
+                icon: Icon(isRunning
+                    ? Icons.pause_circle_outline
+                    : Icons.play_circle_outline),
               ),
             ),
           ),
