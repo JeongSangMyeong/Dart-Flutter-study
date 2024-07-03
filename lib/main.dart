f0ca94021c1612fd3052e7d44e3f73f4f6c4d4f0
JeongSangMyeong <ajflsp@naver.com>
2024-07-03 13:50:34 +0900

    WIdget LifeCycle(initState, dispose, build)

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,13 +30,17 @@ class _AppState extends State<App> {
           ),
         ),
       ),
-      home: const Scaffold(
-        backgroundColor: Color(0xfff4eddb),
+      home: Scaffold(
+        backgroundColor: const Color(0xfff4eddb),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
@@ -37,13 +49,37 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  // 대부분의 경우에서는 필요 없음, 그러나 부모 요소에 의존하는 데이터를 초기화해야 할 때 사용
+  // API에서 업데이트를 구독하고 싶을 때도 사용
+  // 항상 build 메소드보다 먼저 실행되어야 함. 단 한 번만 호출됨.
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  // 위젯이 스크린에서 제거될 때 호출되는 메소드
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return Text(
       'My Large Title',
       style: TextStyle(
