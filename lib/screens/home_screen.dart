b379380b379b0f94904c3f7627cbe0beef402dff
JeongSangMyeong <ajflsp@naver.com>
2024-07-04 16:35:51 +0900

    fetch한 모델 가져오기

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/models/webtoon_model.dart';
+import 'package:webtoon_app/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebtoons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  // build보다 먼저 한 번만 실행.
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     // 기본적인 레이아웃과 설정 제공(Scaffold)
     return Scaffold(
       backgroundColor: Colors.white,
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,10 +4,11 @@ import 'package:http/http.dart' as http;
 import 'package:webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
-  final String today = "today";
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     // 값을 담을 리스트 초기화
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
