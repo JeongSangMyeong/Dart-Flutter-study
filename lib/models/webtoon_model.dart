8055b8b1ae16217d13895e4fe0b7422fa378fed5
JeongSangMyeong <ajflsp@naver.com>
2024-07-04 16:18:54 +0900

    Json 응답 데이터를 model에 담음

--- lib/models/webtoon_model.dart
+++ lib/models/webtoon_model.dart
@@ -1,6 +1,7 @@
 class WebtoonModel {
   final String title, thumb, id;
 
+  // Named Constructure
   WebtoonModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         thumb = json['thumb'],
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,13 +8,18 @@ class ApiService {
   final String today = "today";
 
   Future<List<WebtoonModel>> getTodaysToons() async {
+    // 값을 담을 리스트 초기화
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
+    // http get으로 응답하면 Response가 리턴됨.
     final response = await http.get(url);
     if (response.statusCode == 200) {
+      // List<dynamic>을 안넣어도 되긴 하지만, 리스트라는 것을 명시적으로 보여주기 위해 넣음.
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        // json 응답 데이터를 답음.
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
